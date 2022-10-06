f73ae381f3bf65b4b19e2a44670ac92500e21ad3
OrangeRL <jimkung1@hotmail.com>
2022-10-06 13:05:44 +0900

    Inputのクラス化

---
+++ CG2/Input.h
@@ -0,0 +1,30 @@
+#pragma once
+#define DIRECTINPUT_VERSION 0x0800	//DirectInputVersion
+#include <dinput.h>
+#include "WinApp.h"
+#include<wrl.h>
+#pragma comment(lib,"dinput8.lib")
+#pragma comment(lib,"dxguid.lib")
+
+class Input
+{
+public:
+	Input();
+	~Input();
+
+	IDirectInputDevice8* keyboard;
+	IDirectInput8* directInput;
+
+	unsigned char key[256] = {};
+	unsigned char oldkey[256] = {};
+
+	bool PushKey(unsigned char keys);
+	bool TriggerKey(unsigned char keys);
+	bool ReleaseKey(unsigned char keys);
+
+	void DirectInputInit(WinApp winApp_);
+	void DirectInputCreate(WinApp winApp_);
+
+	void Update();
+
+};
