2adccd0068953d994a4065df63a90311deea3c77
kamisatomasatoshi <k021g1271@g.neec.ac.jp>
2022-10-12 16:07:03 +0900

    no cpp

---
+++ CG2/Resources/GameScene.h
@@ -0,0 +1,26 @@
+#pragma once
+
+class GameScene {
+public:////メンバ関数
+
+	//コンストラクタ
+	GameScene();
+
+	//デストラクタ
+	~GameScene();
+
+
+
+	//初期化
+	void Initialize();
+	//更新処理
+	void Update();
+	//描画
+	void Draw();
+
+	
+private:////メンバ変数
+
+
+
+};
